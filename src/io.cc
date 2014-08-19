a5c42dbd11319153052273f22e7d29350d4b2c6f
ghion.g94@gmail.com <ghion.g94@gmail.com@de8bddb5-3ed4-b0c6-5c16-de49dabef74b>
2014-08-19 00:27:30 +0000

    I file di IO, e logic con i rispettivi header

---
+++ src/io.cc
@@ -0,0 +1,12 @@
+//C++ header
+#include <iostream>
+#include <fstream>
+
+//allgro header
+#include <allegro5/allegro.h>
+
+//header interni
+#include "data_struct.h"
+
+using namespace std;
+
---
+++ src/logic.cc
@@ -0,0 +1,14 @@
+//C++ header
+#include <iostream>
+
+//allegro header
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>	
+#include <allegro5/allegro_image.h>
+
+//header interni
+#include "data_struct.h"
+#include "logic.h"
+#include "io.h"
+
+using namespace std;
